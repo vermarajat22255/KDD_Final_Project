9c802293440659c3221655223d3afa4ca857fc0e
Akhil Manchikanti <akhilm1995@gmail.com>
2020-05-06 16:38:43 -0400

    Support Vector Machine

---
+++ svm_attrition_data.R
@@ -0,0 +1,40 @@
+# remove all objects
+rm(list=ls())
+
+library("e1071")
+
+#input the dataset
+filename <- file.choose()
+dsn <- read.csv(filename)
+dsn_copy <- read.csv(filename)
+
+View(dsn)
+
+#replace all NA in TERMINATION_YEAR to 0
+dsn[c("TERMINATION_YEAR")][is.na(dsn[c("TERMINATION_YEAR")])] <- 0
+
+#remove the STATUS column
+dsn <- within(dsn, rm("STATUS"))
+
+#factoring the data
+dsn$TERMINATION_YEAR <- factor(ifelse(dsn$TERMINATION_YEAR>0, "working", "terminated"))
+
+dev.off()
+
+#SVM
+
+x <- subset(dsn, select=-TERMINATION_YEAR)
+y <- dsn$TERMINATION_YEAR
+
+svm_model <- svm(TERMINATION_YEAR ~ ., data=dsn)
+summary(svm_model)
+
+pred <- predict(svm_model,x)
+
+table(pred,y)
+
+
+SVM_wrong2<-sum(dsn[,14]!=pred)
+error_rate<-SVM_wrong2/length(dsn[,14])
+error_rate
+
