aa357733efb3e50470a35f9a4152efbfba21aa36
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-05-07 00:32:45 -0400

    Updated Kohonen, Table working

--- Kohonen.R
+++ Kohonen.R
@@ -5,6 +5,7 @@ library("kohonen")
 
 employee_data <- read.csv (file ="/Users/meghanabhat/Downloads/attrition_data.csv")
 View(employee_data)
+View(employee_data)
 employee_data$TERMINATION_YEAR[!is.na(employee_data$TERMINATION_YEAR)] <- TRUE
 employee_data$TERMINATION_YEAR[is.na(employee_data$TERMINATION_YEAR)] <- FALSE
 employee_data[employee_data == ""] <- NA
@@ -12,18 +13,21 @@ employee_data <-na.omit(employee_data)
 employee_data <- sapply( employee_data, as.numeric )
 dev.off()
 
+
 ##data is duvided into training data and test data
 
-idx <- sort(sample(nrow(employee_data), as.integer(.70*nrow(employee_data))))
 
-training<-employee_data[idx,]
+training2 <- employee_data[,-14]
+training2 <- training2[,-1]
+training2 <- training2[,-19]
 
-test<-employee_data[-idx,]
 
-employee_data_som <- som(as.matrix(training[-1,-14]), grid = somgrid(3,1))
+employee_data_som <- som(as.matrix(training2), grid = somgrid(2,1))
 
 summary(employee_data_som)
 str(employee_data_som)
 employee_data_som$unit.classif
+
+table(cluster = employee_data_som$unit.classif, employee_data[,21])
 
 
